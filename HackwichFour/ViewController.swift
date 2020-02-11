62e56ed0faabef8572b14efdd027a73997853185
keolaiseri <59892748+keolaiseri@users.noreply.github.com>
2020-02-11 10:31:51 -1000

    Finished in Class

--- HackwichFour/ViewController.swift
+++ HackwichFour/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstLabel: UILabel!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        self.firstLabel.text = "About"
+    
+    
     }
 
 
---
+++ HackwichFour/secondViewController.swift
@@ -0,0 +1,33 @@
+//
+//  secondViewController.swift
+//  HackwichFour
+//
+//  Created by Keola Iseri on 2/11/20.
+//  Copyright © 2020 Keola Iseri. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    
+    @IBOutlet weak var secondLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
